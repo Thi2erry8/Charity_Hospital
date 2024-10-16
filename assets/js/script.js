5708621ad306eb9350ab4f03cd19e719e83e7c39
Thi2erry8 <jonathanaugustin056@gmail.com>
2024-10-15 22:32:50 -0400

    commit_1

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+const preloader = document.querySelector("[data-preloader]");
+window.addEventListener("load" , function (){
+    preloader.classList.add("loaded");
+    document.body.classList.add("loaded");
+});
+//HEADER STICKY
+window.addEventListener("scroll",function(){
+    var header = this.document.querySelector("header");
+    header.classList.toggle("sticky",window.scrollY > 0);
+})
+//Scroll Reveal
+const observer = new IntersectionObserver((entries) =>{
+    entries.forEach((entry) =>{
+      console.log(entry)
+      if(entry.isIntersecting){
+        entry.target.classList.add('show');
+      }else{
+        entry.target.classList.remove('show');
+      }
+    })
+})
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
